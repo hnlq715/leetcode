edb3216029f7e8e516fc80abee5e9fceec267dfb
hnlq715 <hnlq.sysu@gmail.com>
2020-02-14 15:16:27 +0800

    two sum

--- 1.两数之和.go
+++ 1.两数之和.go
@@ -6,7 +6,16 @@
 
 // @lc code=start
 func twoSum(nums []int, target int) []int {
-    
+	pivot := make(map[int]int)
+	for i, num := range nums {
+		find := target - num
+		if idx, ok := pivot[find]; ok {
+			return []int{idx,i}
+		}
+		pivot[num] = i
+	}
+
+	return nil
 }
 // @lc code=end
 
